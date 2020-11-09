ce55bcf0667d5267359c05a362f153ddac37f990
corbaz <julio.corbaz@gmail.com>
2020-11-09 09:41:46 -0300

    version: 20.11.09.0941

---
+++ docs/svelte.md
@@ -0,0 +1,12 @@
+# Svelte
+
+1. Julio
+2. Cesar
+3. Corbaz
+
+<img src="https://picsum.photos/id/237/600/400" alt="perro">
+
+![ciudad](https://picsum.photos/id/238/600/400 "Ciudad")
+
+https://www.webfx.com/tools/emoji-cheat-sheet/
+
---
+++ gz.bat
@@ -0,0 +1,26 @@
+cls
+@echo off
+set HORA_COMPLETA=%TIME%
+set HORA=%HORA_COMPLETA:~0,2%
+if %HORA% LSS 10 set HORA=0%HORA_COMPLETA:~1,1%
+set MINUTOS=%HORA_COMPLETA:~3,2%
+set SEGUNDOS=%HORA_COMPLETA:~6,2%
+
+set FECHA_COMPLETA=%DATE%
+set DIA=%FECHA_COMPLETA:~0,2%
+set MES=%FECHA_COMPLETA:~3,2%
+set ANIO=%FECHA_COMPLETA:~6,2%
+
+set Version=%Anio%.%MES%.%DIA%.%HORA%%MINUTOS%
+@echo version: %Version%
+rem del C:\www\0-notebo-ok\src\js\version.js
+rem echo export let version = "%Version%";> C:\www\0-notebo-ok\src\js\version.js
+
+rem call npm run build
+
+git status -s -b
+git add .
+git commit -am "version: %Version%"
+
+rem git push --set-upstream origin master --force
+git push -u origin main --force"
